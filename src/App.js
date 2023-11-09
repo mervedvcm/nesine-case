5d52896d203e9d2ac3e01b80721ff209a78ae4de
Merve Deveci <mervedeveci1@hotmail.com>
2023-11-09 17:46:18 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import BulletinBoard from './BulletinBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <BulletinBoard />
+    </div>
+  );
+}
+
+export default App;
